68c77732588f5e2ee9ac412360695827485e9330
Alex Kott <tetlokax@gmail.com>
2017-11-16 00:26:40 +0700

    add express server for static files

--- src/Server.js
+++ src/Server.js
@@ -1,5 +1,36 @@
-/*
-SERVER
-  1. Setup web server
-  2. Serve index.html with react script as static assets (for jsdom)
-*/
+/***
+  The server is opening all static files in the buildPath (especially
+  important for the *.js files).
+  For all accessed routes it returns the temporary (webpack) html file, so
+  that the frontend router can take over.
+***/
+
+import express from 'express';
+import path from 'path';
+
+export default class Server {
+  constructor(buildDir = 'build') {
+    this.PORT = 3231;
+    const buildPath = path.join(process.cwd(), buildDir);
+    const app = express();
+
+    app.use(express.static(buildPath));
+    app.get('*', (req, res) => res.sendFile(path.join(buildPath, '_tmp.html')));
+
+    this.app = app;
+  }
+
+  getPort() {
+    return this.instance.address().port;
+  }
+
+  start() {
+    this.instance = this.app.listen(this.PORT, () => {
+      console.log('Server is listening...');
+    });
+  }
+
+  stop() {
+    this.instance.close();
+  }
+}
--- src/cli.js
+++ src/cli.js
@@ -6,10 +6,18 @@ CLI
   if (!fs.existsSync(buildDir)) throw new Error(`No build directory exists at: ${buildDirPath}`);
 
   3. create Writer
-  4. create Server
-  5. create Crawler
-  6. Crawler crawl
-  7. Take result html and path
-  8. Writer write to file
-  9. Stop Sever
+  4. rename index.html -> _tmp.html
+  5. create Server
+  6. create Crawler
+  7. Crawler crawl
+  8. Take result html and path
+  9. Writer write to file
+  10. Stop Sever
+  11. remove _tmp.html
 */
+import Server from './Server';
+
+export default (() => {
+  const server = new Server();
+  server.start();
+})();
